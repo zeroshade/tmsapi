106338132b54ffbf1d18998f6f5f350ffe9e4c56
Matthew Topol <mtopol@factset.com>
2020-11-22 18:49:32 -0500

    add fromnumber to the merchant_config

--- types/merchant_config.go
+++ types/merchant_config.go
@@ -8,17 +8,18 @@ type SandboxInfo struct {
 }
 
 type MerchantConfig struct {
-	ID              string `json:"-" gorm:"primary_key"`
-	PassTitle       string `json:"passTitle"`
-	NotifyNumber    string `json:"notifyNumber"`
-	EmailFrom       string `json:"emailFrom"`
-	EmailName       string `json:"emailName"`
-	EmailContent    string `json:"emailContent"`
-	SendSMS         bool   `json:"sendSMS" gorm:"default:false"`
-	TermsConds      string `json:"terms"`
-	SandboxID       string `json:"-"`
-	TwilioAcctSID   string `json:"-"`
-	TwilioAcctToken string `json:"-"`
-	StripeKey       string `json:"-"`
-	PaymentType     string `json:"-"`
+	ID               string `json:"-" gorm:"primary_key"`
+	PassTitle        string `json:"passTitle"`
+	NotifyNumber     string `json:"notifyNumber"`
+	EmailFrom        string `json:"emailFrom"`
+	EmailName        string `json:"emailName"`
+	EmailContent     string `json:"emailContent"`
+	SendSMS          bool   `json:"sendSMS" gorm:"default:false"`
+	TermsConds       string `json:"terms"`
+	SandboxID        string `json:"-"`
+	TwilioAcctSID    string `json:"-"`
+	TwilioAcctToken  string `json:"-"`
+	TwilioFromNumber string `json:"-"`
+	StripeKey        string `json:"-"`
+	PaymentType      string `json:"-"`
 }
