131a8b38a0cc0ec161e24cb3f608e50bbf766b10
Matthew Topol <mtopol@factset.com>
2021-01-31 16:28:25 -0500

    some logging

--- stripe/handler.go
+++ stripe/handler.go
@@ -262,12 +262,15 @@ func (h Handler) TransferTickets(_ *types.MerchantConfig, db *gorm.DB, data []ty
 			Where("li.id = ?", data[idx].LineItemID).
 			Select("li.quantity", "coalesce(new_sku, sku) AS sku").Scan(&r)
 
+		fmt.Println(r)
 		re := regexp.MustCompile(`(\d+)[A-Z]+(\d{10})`)
 		result := re.FindStringSubmatch(r[0].Sku)
+		fmt.Println(result)
 		oldPid, _ := strconv.Atoi(result[1])
 		oldTm, _ := strconv.ParseInt(result[2], 10, 64)
 
 		result = re.FindStringSubmatch(data[idx].NewSKU)
+		fmt.Println(result)
 		newPid, _ := strconv.Atoi(result[1])
 		newTm, _ := strconv.ParseInt(result[2], 10, 64)
 		db.Table("manual_overrides").Where("product_id = ? AND time = TO_TIMESTAMP(?::INTEGER)", oldPid, oldTm).
