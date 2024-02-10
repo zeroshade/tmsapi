13621b17ee6eed29574ff4b2f0eb1c928436c7fd
Matt Topol <zotthewizard@gmail.com>
2024-02-10 13:35:07 -0500

    add boat_override

--- types/product.go
+++ types/product.go
@@ -11,20 +11,21 @@ type Boat struct {
 
 // Product represents a specific Type of tickets sold
 type Product struct {
-	ID          uint       `json:"id" gorm:"primary_key"`
-	StripeID    string     `json:"stripeId" gorm:"-"`
-	MerchantID  string     `json:"-" gorm:"type:varchar;not null;primary_key;"`
-	CreatedAt   time.Time  `json:"-"`
-	UpdatedAt   time.Time  `json:"-"`
-	DeletedAt   *time.Time `json:"-"`
-	Name        string     `json:"name"`
-	Desc        string     `json:"desc"`
-	Color       string     `json:"color"`
-	Publish     bool       `json:"publish"`
-	ShowTickets bool       `json:"showTickets"`
-	Schedules   []Schedule `json:"schedList"`
-	Fish        string     `json:"fish"`
-	Boat        *Boat      `json:"-"`
-	BoatID      uint       `json:"boatId" gorm:"default:1"`
-	Type        string     `json:"type" gorm:"default:'trip'"`
+	ID           uint       `json:"id" gorm:"primary_key"`
+	StripeID     string     `json:"stripeId" gorm:"-"`
+	MerchantID   string     `json:"-" gorm:"type:varchar;not null;primary_key;"`
+	CreatedAt    time.Time  `json:"-"`
+	UpdatedAt    time.Time  `json:"-"`
+	DeletedAt    *time.Time `json:"-"`
+	Name         string     `json:"name"`
+	Desc         string     `json:"desc"`
+	Color        string     `json:"color"`
+	Publish      bool       `json:"publish"`
+	ShowTickets  bool       `json:"showTickets"`
+	Schedules    []Schedule `json:"schedList"`
+	Fish         string     `json:"fish"`
+	Boat         *Boat      `json:"-"`
+	BoatID       uint       `json:"boatId" gorm:"default:1"`
+	Type         string     `json:"type" gorm:"default:'trip'"`
+	BoatOverride bool       `json:"boatOverride" gorm:"default:false"`
 }
